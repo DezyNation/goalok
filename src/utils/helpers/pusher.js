52096afd1499ca8d234cfb9b56fa837885d45b4d
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-09-16 21:40:14 +0530

    force tls

--- src/utils/helpers/pusher.js
+++ src/utils/helpers/pusher.js
@@ -3,7 +3,7 @@ const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
     wsHost: process.env.NEXT_PUBLIC_PUSHER_BASE_URL,
     wsPort: 6001,
     wssPort: 6001,
-    forceTLS: false,
+    forceTLS: true,
     enabledTransports: ['wss'],
     
 });
