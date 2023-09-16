10eba472306e1ce7efdd31c3ad70f9444a412c68
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-09-16 20:24:20 +0530

    ws configuration

--- src/utils/helpers/pusher.js
+++ src/utils/helpers/pusher.js
@@ -1,6 +1,9 @@
 import Pusher from "pusher-js";
 const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
     wsHost: process.env.NEXT_PUBLIC_PUSHER_BASE_URL,
+    wssHost: process.env.NEXT_PUBLIC_PUSHER_BASE_URL,
+    wssPort: 6001,
+    forceTLS: true,
     enabledTransports: ['wss']
 });
 
