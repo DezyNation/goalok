def16794c6f71b6765a5393fdd69b2aa935c804a
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-09-16 20:59:45 +0530

    tls false

--- src/utils/helpers/pusher.js
+++ src/utils/helpers/pusher.js
@@ -2,9 +2,12 @@ import Pusher from "pusher-js";
 const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
     wsHost: process.env.NEXT_PUBLIC_PUSHER_BASE_URL,
     wssHost: process.env.NEXT_PUBLIC_PUSHER_BASE_URL,
+    httpHost: process.env.NEXT_PUBLIC_PUSHER_BASE_URL,
     wssPort: 6001,
-    forceTLS: true,
-    enabledTransports: ['wss']
+    wssPort: 6001,
+    forceTLS: false,
+    enabledTransports: ['wss'],
+    
 });
 
 export default pusher
