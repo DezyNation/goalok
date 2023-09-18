fcbe4509f17d1e2bd0cac680489054b393dd8d41
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-09-19 03:13:25 +0530

    wsport removed

--- src/utils/helpers/pusher.js
+++ src/utils/helpers/pusher.js
@@ -1,8 +1,6 @@
 import Pusher from "pusher-js";
 const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
     wsHost: process.env.NEXT_PUBLIC_PUSHER_BASE_URL,
-    wsPort: 443,
-    wssPort: 443,
     forceTLS: true,
     enabledTransports: ['wss', 'ws'],
     
