f400c240497aa095e16c5239837254857d7ac070
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-09-19 01:00:24 +0530

    hhtp

--- src/utils/helpers/pusher.js
+++ src/utils/helpers/pusher.js
@@ -1,6 +1,8 @@
 import Pusher from "pusher-js";
 const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
     wsHost: process.env.NEXT_PUBLIC_PUSHER_BASE_URL,
+    httpHost: process.env.NEXT_PUBLIC_PUSHER_BASE_URL,
+    httpPORT: 443,
     wsPort: 443,
     wssPort: 443,
     forceTLS: true,
