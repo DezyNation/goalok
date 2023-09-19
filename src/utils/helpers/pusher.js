25474afae1e1262b2ad86320d9fce007ddf1b5f0
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-09-19 09:52:43 +0530

    ports added

--- src/utils/helpers/pusher.js
+++ src/utils/helpers/pusher.js
@@ -1,6 +1,8 @@
 import Pusher from "pusher-js";
 const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
     wsHost: process.env.NEXT_PUBLIC_PUSHER_BASE_URL,
+    wsPort: 443,
+    wssPort: 443,
     forceTLS: true,
     enabledTransports: ['wss', 'ws'],
     
