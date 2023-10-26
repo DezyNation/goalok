8523075bbe24805ae152f47694ef7afa5b138b28
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-10-26 16:22:05 +0530

    exp: store url styling fix

--- src/app/store/page.jsx
+++ src/app/store/page.jsx
@@ -5,15 +5,13 @@ import React from "react";
 const page = () => {
   return (
     <>
-      <Box w={"full"} h={"100vh"}>
         <iframe
           src="https://iskconincstore.com"
           frameborder="0"
-          width={"100%"}
-          height={"100%"}
-          style={{ width: "100%", height: "100%" }}
+          width={"100vw"}
+          height={"100vh"}
+          style={{ width: "100vw", height: "100vh" }}
         ></iframe>
-      </Box>
     </>
   );
 };
