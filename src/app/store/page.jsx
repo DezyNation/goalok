f229247fa867f6cd32ca6869aaa17296e695a5da
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-10-26 16:17:35 +0530

    exp: store URL added

---
+++ src/app/store/page.jsx
@@ -0,0 +1,21 @@
+"use client";
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Box w={"full"} h={"100vh"}>
+        <iframe
+          src="https://iskconincstore.com"
+          frameborder="0"
+          width={"100%"}
+          height={"100%"}
+          style={{ width: "100%", height: "100%" }}
+        ></iframe>
+      </Box>
+    </>
+  );
+};
+
+export default page;
