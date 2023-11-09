14bbf1d546de20879a0e889036ce3a2de39506d4
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-11-09 14:27:32 +0530

    auto subscribe enabled

--- src/app/dashboard/sessions/join/[slug]/page.tsx
+++ src/app/dashboard/sessions/join/[slug]/page.tsx
@@ -64,7 +64,7 @@ export default function Page({ params }) {
         video={true}
         audio={true}
         token={token}
-        connectOptions={{ autoSubscribe: false, maxRetries: 3 }}
+        connectOptions={{ autoSubscribe: true, maxRetries: 3 }}
         serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
         data-lk-theme="default"
         style={{ height: "100dvh" }}
