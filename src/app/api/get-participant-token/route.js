38f27f6dff41d0fc1fefea174527c62ad8990661
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-11-09 14:12:47 +0530

    sources fixed

--- src/app/api/get-participant-token/route.js
+++ src/app/api/get-participant-token/route.js
@@ -2,50 +2,47 @@ import { AccessToken } from "livekit-server-sdk";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req) {
-    const room = req.nextUrl.searchParams.get("room");
-    const username = req.nextUrl.searchParams.get("username");
-    const role = req.nextUrl.searchParams.get("role");
+  const room = req.nextUrl.searchParams.get("room");
+  const username = req.nextUrl.searchParams.get("username");
+  const role = req.nextUrl.searchParams.get("role");
 
-    if (!room) {
-      return NextResponse.json(
-        { error: 'Missing "room" query parameter' },
-        { status: 400 }
-      );
-    } else if (!username) {
-      return NextResponse.json(
-        { error: 'Missing "username" query parameter' },
-        { status: 400 }
-      );
-    }
+  if (!room) {
+    return NextResponse.json(
+      { error: 'Missing "room" query parameter' },
+      { status: 400 }
+    );
+  } else if (!username) {
+    return NextResponse.json(
+      { error: 'Missing "username" query parameter' },
+      { status: 400 }
+    );
+  }
 
-    const apiKey = process.env.LIVEKIT_API_KEY;
-    const apiSecret = process.env.LIVEKIT_API_SECRET;
-    const wsUrl = process.env.NEXT_PUBLIC_LIVEKIT_URL;
+  const apiKey = process.env.LIVEKIT_API_KEY;
+  const apiSecret = process.env.LIVEKIT_API_SECRET;
+  const wsUrl = process.env.NEXT_PUBLIC_LIVEKIT_URL;
 
-    if (!apiKey || !apiSecret || !wsUrl) {
-      return NextResponse.json(
-        { error: "Server misconfigured" },
-        { status: 500 }
-      );
-    }
+  if (!apiKey || !apiSecret || !wsUrl) {
+    return NextResponse.json(
+      { error: "Server misconfigured" },
+      { status: 500 }
+    );
+  }
 
-    const at = new AccessToken(apiKey, apiSecret, { identity: username });
-    const isHost = role == "admin" || role == "preacher";
+  const at = new AccessToken(apiKey, apiSecret, { identity: username });
+  const isHost = role == "admin" || role == "preacher";
 
-    at.addGrant({
-      room,
-      roomJoin: true,
-      canPublish: true,
-      canSubscribe: true,
-      canPublishData: true,
-      roomRecord: true,
-      roomAdmin: isHost,
-      canPublishSources: [
-        "camera",
-        "microphone",
-        isHost && "screen_share",
-        isHost && "screen_share_audio",
-      ],
-    });
-    return NextResponse.json({ token: at.toJwt() });
+  at.addGrant({
+    room,
+    roomJoin: true,
+    canPublish: true,
+    canSubscribe: true,
+    canPublishData: true,
+    roomRecord: true,
+    roomAdmin: isHost,
+    canPublishSources: isHost
+      ? ["camera", "microphone"]
+      : ["camera", "microphone", "screen_share", "screen_share_audio"],
+  });
+  return NextResponse.json({ token: at.toJwt() });
 }
