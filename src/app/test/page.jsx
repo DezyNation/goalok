c24223f702db1473647b27ca2d10dc65d98dfc90
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-12-06 19:59:37 +0530

    audio testing

---
+++ src/app/test/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import AudioBtn from "../../components/counselling/AudioBtn";
+
+const page = () => {
+  return (
+    <>
+      <Box>
+        <AudioBtn />
+      </Box>
+    </>
+  );
+};
+
+export default page;
---
+++ src/components/counselling/AudioBtn.jsx
@@ -0,0 +1,62 @@
+"use client";
+import { Box, Image } from "@chakra-ui/react";
+import Lottie from "lottie-react";
+import React, { useEffect, useRef, useState } from "react";
+import blob from "../../../public/lottie/blob.json";
+
+const AudioBtn = () => {
+  const [audioPlaying, setAudioPlaying] = useState(true);
+  const audioref = useRef(null);
+
+  useEffect(() => {
+    play();
+  }, []);
+
+  function play(volume) {
+    audioref?.current?.play();
+    audioref.current.volume = 0.025
+    setAudioPlaying(true);
+  }
+
+  function pause() {
+    audioref?.current?.pause();
+    setAudioPlaying(false);
+  }
+
+  return (
+    <>
+      <audio ref={audioref} autoPlay>
+        <source src={"/audios/kirtan.mp3"} type="audio/mpeg"></source>
+      </audio>
+      <Box w={24} h={24} pos={"fixed"} bottom={4} left={4}>
+        <Lottie
+          animationData={blob}
+          loop
+          autoPlay
+          width={"full"}
+          height={"full"}
+        />
+        <Box
+          pos={"absolute"}
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          display={"grid"}
+          placeContent={"center"}
+          cursor={"pointer"}
+          onClick={() => (audioPlaying ? pause() : play())}
+        >
+          <Image
+            src={audioPlaying ? "/icons/pause.png" : "/icons/play.png"}
+            width={5}
+            height={5}
+            objectFit={"contain"}
+          />
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default AudioBtn;
