c4b64892ad8a1b2985015e3bf7b863c5534dd400
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-12-07 13:59:50 +0530

    hide audio for ios

--- src/components/counselling/AudioBtn.jsx
+++ src/components/counselling/AudioBtn.jsx
@@ -6,14 +6,19 @@ import blob from "../../../public/lottie/blob.json";
 
 const AudioBtn = () => {
   const [audioPlaying, setAudioPlaying] = useState(false);
+  const [isIos, setIsIos] = useState(false);
   const audioref = useRef(null);
 
-  // useEffect(() => {
-  //   play();
-  // }, []);
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+
+    if (userAgent.match(/iPhone/i)) {
+      setIsIos(true);
+    }
+  }, []);
 
   function play(volume) {
-    audioref.current.volume = 0.025
+    audioref.current.volume = 0.025;
     audioref?.current?.play();
     setAudioPlaying(true);
   }
@@ -25,36 +30,38 @@ const AudioBtn = () => {
 
   return (
     <>
-      <audio ref={audioref} autoPlay>
+      <audio ref={audioref}>
         <source src={"/audios/kirtan.mp3"} type="audio/mpeg"></source>
       </audio>
-      <Box w={24} h={24} pos={"fixed"} bottom={4} left={4}>
-        <Lottie
-          animationData={blob}
-          loop
-          autoPlay
-          width={"full"}
-          height={"full"}
-        />
-        <Box
-          pos={"absolute"}
-          top={0}
-          left={0}
-          right={0}
-          bottom={0}
-          display={"grid"}
-          placeContent={"center"}
-          cursor={"pointer"}
-          onClick={() => (audioPlaying ? pause() : play())}
-        >
-          <Image
-            src={audioPlaying ? "/icons/pause.png" : "/icons/play.png"}
-            width={5}
-            height={5}
-            objectFit={"contain"}
+      {isIos ? null : (
+        <Box w={24} h={24} pos={"fixed"} bottom={4} left={4}>
+          <Lottie
+            animationData={blob}
+            loop
+            autoPlay
+            width={"full"}
+            height={"full"}
           />
+          <Box
+            pos={"absolute"}
+            top={0}
+            left={0}
+            right={0}
+            bottom={0}
+            display={"grid"}
+            placeContent={"center"}
+            cursor={"pointer"}
+            onClick={() => (audioPlaying ? pause() : play())}
+          >
+            <Image
+              src={audioPlaying ? "/icons/pause.png" : "/icons/play.png"}
+              width={5}
+              height={5}
+              objectFit={"contain"}
+            />
+          </Box>
         </Box>
-      </Box>
+      )}
     </>
   );
 };
